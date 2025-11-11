c73a4a9cc71796c8588af0a6a4a181cf2fc45474
hujiacheng <hujiacheng@iipcloud.onaliyun.com>
2025-11-11 16:28:47 +0800

    Integrate vite-plugin-dts into Vue3 Best UI package for TypeScript declaration file generation; update tsconfig.json to extend base configuration and modify package.json to include vite-plugin-dts as a dependency.

--- packages/vue3-best-ui/vite.config.ts
+++ packages/vue3-best-ui/vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import dts from 'vite-plugin-dts'
 import { resolve } from 'path';
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), dts({
+    insertTypesEntry: true,
+    outDir: 'dist',
+    rollupTypes: true,
+    copyDtsFiles: true,
+    staticImport: true,
+    clearPureImport: true,
+    include: ['src/**/*', 'index.ts']
+  })],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
