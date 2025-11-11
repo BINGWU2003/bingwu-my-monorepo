0289f087723970f2278e2f5e87048e0c8b157d4f
hujiacheng <hujiacheng@iipcloud.onaliyun.com>
2025-11-11 16:22:32 +0800

    Enhance Button component in Vue3 Best UI by adding unique ID and formatted date display; integrate utility functions for ID generation and date formatting.

--- packages/vue3-best-ui/src/Button/index.vue
+++ packages/vue3-best-ui/src/Button/index.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="button">
     <slot></slot>
-    <button @click="emit('click')">{{ props.text }}</button>
+    <button @click="emit('click')">{{ props.text }} {{ id }} {{ date }}</button>
   </div>
 </template>
 
 <script setup lang="ts">
 import type { ButtonProps, ButtonEmits } from "./types";
-
+import { generateId, formatDate } from "@bingwu-my-monorepo/shared";
 const props = withDefaults(defineProps<ButtonProps>(), {
   text: "Button",
 });
 const emit = defineEmits<ButtonEmits>();
+const id = generateId();
+const date = formatDate(new Date());
 </script>
 
 <style scoped lang="scss">
